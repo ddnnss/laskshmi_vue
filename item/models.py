d9e6c6bbb5cc4fed86490605fe626a5f22e4d1ef
ddnnss <ddnnss.i1@gmail.com>
2020-05-04 20:48:28 +0300

    Initial commit

---
+++ item/models.py
@@ -0,0 +1,355 @@
+from django.db import models
+from django.utils import timezone
+from pytils.translit import slugify
+from PIL import Image
+from django.db.models.signals import post_save
+import uuid
+from random import choices
+import string
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.conf import settings
+from django.utils.safestring import mark_safe
+#from laskshmi import settings
+
+import os
+
+
+def format_number(num):
+    if num % 1 == 0:
+        return int(num)
+    else:
+        return num
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=255, blank=False, null=True)
+    name_slug = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField('Изображение категории', upload_to='category_img/', blank=False)
+    icon = models.ImageField('Иконка категории', upload_to='category_img/', blank=True,null=True)
+    page_title = models.CharField('Название страницы', max_length=255, blank=False, null=True)
+    page_description = models.CharField('Описание страницы', max_length=255, blank=False, null=True)
+    page_keywords = models.TextField('Keywords', blank=False, null=True)
+    short_description = models.TextField('Краткое описание для главной', blank=True,)
+    description = RichTextUploadingField('Описание категории', blank=True, null=True)
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.name_slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return 'id :%s , %s ' % (self.id, self.name)
+
+    def get_icon(self):
+        if self.icon:
+            return self.icon.url
+        else:
+            return 'https://placehold.it/40'
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, blank=False, null=True, on_delete=models.SET_NULL)
+    name = models.CharField('Название подкатегории', max_length=255, blank=False, null=True)
+    name_slug = models.CharField(max_length=255, blank=True, null=True)
+    icon = models.ImageField('Иконка категории', upload_to='category_img/', blank=True, null=True)
+    image = models.ImageField('Изображение подкатегории', upload_to='sub_category_img/', blank=False)
+    page_title = models.CharField('Название страницы', max_length=255, blank=False, null=True)
+    page_description = models.CharField('Описание страницы', max_length=255, blank=False, null=True)
+    page_keywords = models.TextField('Keywords', blank=False, null=True)
+    description = RichTextUploadingField('Описание подкатегории', blank=True, null=True)
+    discount = models.IntegerField('Скидка на все товары в подкатегории %', blank=True, default=0)
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.name_slug = slugify(self.name)
+        all_items = self.item_set.all()
+        for item in all_items:
+            item.discount = self.discount
+            item.save()
+
+        super(SubCategory, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return 'id :%s , %s ' % (self.id, self.name)
+
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Подкатегории"
+
+
+class Filter(models.Model):
+    subcategory = models.ForeignKey(SubCategory, blank=True, null=True,on_delete=models.SET_NULL, verbose_name='Подкатегория')
+    name = models.CharField('Название фильтра', max_length=255, blank=False, null=True)
+    name_slug = models.CharField(max_length=255, blank=True, null=True)
+    seoText = RichTextUploadingField('Текст для СЕО', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.name_slug = slugify(self.name)
+        super(Filter, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '%s | %s ' % (self.subcategory.name, self.name)
+
+    class Meta:
+        verbose_name = "Фильтр"
+        verbose_name_plural = "Фильтры"
+
+class Collection(models.Model):
+    category = models.ForeignKey(Category, blank=False, null=True, on_delete=models.SET_NULL, verbose_name='Категория')
+    name = models.CharField('Название коллекции', max_length=255, blank=False, null=True)
+    icon = models.ImageField('Иконка категории', upload_to='category_img/', blank=True, null=True)
+    name_slug = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField('Изображение коллекции', upload_to='collection_img/', blank=False)
+    page_title = models.CharField('Название страницы', max_length=255, blank=False, null=True)
+    page_description = models.TextField('Описание страницы', blank=False, null=True)
+    page_keywords = models.TextField('Keywords', blank=False, null=True)
+    description = RichTextUploadingField('Описание коллекции', blank=True, null=True)
+    discount = models.IntegerField('Скидка на все товары в коллекции %', blank=True, default=0)
+    views = models.IntegerField(default=0)
+    show_at_homepage = models.BooleanField('Отображать на главной', default=True)
+    show_at_category = models.BooleanField('Отображать в категории', default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.name_slug = slugify(self.name)
+        all_items = Item.objects.filter(collection=self)
+        for item in all_items:
+            item.discount = self.discount
+            item.save()
+        super(Collection, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '%s ' % self.name
+
+    class Meta:
+        verbose_name = "Коллекция"
+        verbose_name_plural = "Коллекции"
+
+class Item(models.Model):
+    collection = models.ManyToManyField(Collection, blank=True, verbose_name='Коллекция',db_index=True)
+    filter = models.ForeignKey(Filter, blank=True, null=True, on_delete=models.SET_NULL,db_index=True)
+    subcategory = models.ForeignKey(SubCategory, blank=False, null=True, verbose_name='Подкатегория', on_delete=models.SET_NULL,db_index=True)
+    name = models.CharField('Название товара', max_length=255, blank=False, null=True)
+    name_lower = models.CharField(max_length=255, blank=True, null=True,default='')
+    name_slug = models.CharField(max_length=255, blank=True, null=True,db_index=True)
+    price = models.IntegerField('Цена', blank=False, default=0, db_index=True)
+    discount = models.IntegerField('Скидка %', blank=True, default=0, db_index=True)
+    page_title = models.CharField('Название страницы', max_length=255, blank=False, null=True)
+    page_description = models.TextField('Описание страницы',  blank=False, null=True)
+    description = models.TextField('Описание товара', blank=True, null=True)
+    comment = models.TextField('Комментарий', blank=True, null=True)
+    length = models.CharField('Длина', max_length=15, default='не указано')
+    width = models.CharField('Ширина', max_length=15, default='не указано')
+    height = models.CharField('Высота',  max_length=15, default='не указано')
+    article = models.CharField('Артикул', max_length=50, blank=False, null=True)
+    weight = models.CharField('Вес',  max_length=15, default='не указано')
+    material = models.CharField('Материал', max_length=50, blank=True, null=True, default='не указано')
+    is_active = models.BooleanField('Отображать товар ?', default=True, db_index=True)
+    is_present = models.BooleanField('Товар в наличии ?', default=True, db_index=True)
+    is_new = models.BooleanField('Товар новинка ?', default=False, db_index=True)
+    is_reserved = models.BooleanField('Товар в резерве ?', default=False)
+    buys = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.name_slug = slugify(self.name)
+        self.name_lower = self.name.lower()
+        super(Item, self).save(*args, **kwargs)
+
+    def getfirstimage(self):
+        url = None
+        for img in self.itemimage_set.all():
+            if img.is_main:
+                url = img.image_small
+        return url
+
+    def image_tag(self):
+        # used in the admin site model as a "thumbnail"
+        if self.getfirstimage():
+            return mark_safe('<img src="{}" width="100" height="100" />'.format(self.getfirstimage()))
+        else:
+            return mark_safe('<span>НЕТ МИНИАТЮРЫ</span>')
+
+    image_tag.short_description = 'Основная картинка'
+
+
+
+    @property
+    def discount_value(self):
+        if self.discount > 0:
+            dis_val = self.price - (self.price * self.discount / 100)
+        else:
+            dis_val = self.price
+        return (round(dis_val))
+
+
+    def __str__(self):
+        if self.filter:
+            return 'id:%s %s | Фильтр %s' % (self.id, self.name, self.filter.name)
+        else:
+            return 'id:%s %s | Фильтра нет' % (self.id, self.name)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
+
+class ItemImage(models.Model):
+    upload_to = 'items/%d/%s'
+
+
+    def _get_upload_to(self, filename):
+        ext = filename.split('.')[-1]
+
+        filename = '{}.{}'.format(self.item.pk, ext)
+
+        return self.upload_to % (self.item.id, filename)
+
+    item = models.ForeignKey(Item, blank=False, null=True, on_delete=models.CASCADE, verbose_name='Товар')
+    image = models.ImageField('Изображение товара', upload_to=upload_to, blank=False)
+    image_small = models.CharField(max_length=255, blank=True, default='')
+    f_id = models.CharField(max_length=5, blank=True, default='')
+    is_main = models.BooleanField('Основная картинка ?', default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s Изображение для товара : %s ' % (self.id, self.item.name)
+
+    class Meta:
+        verbose_name = "Изображение для товара"
+        verbose_name_plural = "Изображения для товара"
+
+    def image_tag(self):
+        # used in the admin site model as a "thumbnail"
+        if self.image_small:
+            return mark_safe('<img src="{}" width="150" height="150" />'.format(self.image_small))
+        else:
+            return mark_safe('<span>НЕТ МИНИАТЮРЫ</span>')
+
+    image_tag.short_description = 'Картинка'
+
+
+    def save(self, *args, **kwargs):
+        fill_color = '#fff'
+        base_image = Image.open(self.image)
+
+        if base_image.mode in ('RGBA', 'LA'):
+            background = Image.new(base_image.mode[:-1], base_image.size, fill_color)
+            background.paste(base_image, base_image.split()[-1])
+            base_image = background
+        os.makedirs('media/items/{}'.format(self.item.id), exist_ok=True)
+        watermark = Image.open('static/images/watermark30.png')
+        width, height = base_image.size
+        transparent = Image.new('RGB', (width, height), (0, 0, 0, 0))
+        transparent.paste(base_image, (0, 0))
+        transparent.paste(watermark, (291, 386), mask=watermark)
+        # transparent.show()
+        image_url = 'media/items/{}/{}'.format(self.item.id, str(uuid.uuid4()) + '_watermarked.jpg')
+        if settings.DEBUG:
+            transparent.save(image_url, 'JPEG', quality=80)
+        else:
+            transparent.save('laskshmi/' + image_url, 'JPEG', quality=80)
+        original_image_url = 'media/items/{}/{}'.format(self.item.id, str(uuid.uuid4()) + '_original.jpg')
+        if settings.DEBUG:
+            base_image.save(original_image_url, 'JPEG', quality=80)
+        else:
+            base_image.save('laskshmi/' + original_image_url, 'JPEG', quality=80)
+        # transparent.save(image_url, 'JPEG', quality=80)
+        self.image = '/' + image_url
+
+
+        # if image.mode in ('RGBA', 'LA'):
+        #     background = Image.new(image.mode[:-1], image.size, fill_color)
+        #     background.paste(image, image.split()[-1])
+        #     image = background
+        transparent.thumbnail((400, 400), Image.ANTIALIAS)
+        small_name = 'media/items/{}/{}'.format(self.item.id, str(uuid.uuid4()) + '.jpg')
+        if settings.DEBUG:
+            transparent.save(small_name, 'JPEG', quality=75)
+        else:
+            transparent.save('laskshmi/' + small_name, 'JPEG', quality=75)
+        self.image_small = '/' + small_name
+
+        super(ItemImage, self).save(*args, **kwargs)
+
+
+
+class PromoCode(models.Model):
+    promo_code = models.CharField('Промокод (для создания рандомного значения оставить пустым)', max_length=255, blank=True, null=True)
+    promo_discount = models.IntegerField('Скидка на заказ', blank=False, default=0)
+    use_counts = models.IntegerField('Кол-во использований', blank=True, default=1)
+    is_unlimited = models.BooleanField('Неограниченное кол-во использований', default=False)
+    is_active = models.BooleanField('Активен?', default=True)
+    expiry = models.DateTimeField('Срок действия безлимитного кода', blank=True, null=True)
+
+    def __str__(self):
+        if self.is_unlimited:
+            return 'Неограниченный промокод со скидкой : %s . Срок действия до : %s' % (self.promo_discount, self.expiry)
+        else:
+            return 'Ограниченный промокод со скидкой : %s . Оставшееся кол-во использований : %s' % (self.promo_discount, self.use_counts)
+
+    class Meta:
+        verbose_name = "Промокод"
+        verbose_name_plural = "Промокоды"
+
+    def save(self, *args, **kwargs):
+        if self.is_unlimited:
+            if not self.promo_code:
+                self.promo_code = "LM-"+''.join(choices(string.ascii_uppercase + string.digits, k=5))
+                self.use_counts = 0
+        else:
+            if not self.promo_code:
+                self.promo_code = "LM-" + ''.join(choices(string.ascii_uppercase + string.digits, k=5))
+
+
+        super(PromoCode, self).save(*args, **kwargs)
+
+
+
+
+def ItemImage_post_save(sender,instance,**kwargs):
+    base_image = Image.open(instance.image)
+    fill_color = '#fff'
+    os.remove(instance.image.url)
+    if base_image.mode in ('RGBA', 'LA'):
+        background = Image.new(base_image.mode[:-1], base_image.size, fill_color)
+        background.paste(base_image, base_image.split()[-1])
+        base_image = background
+    watermark = Image.open('static/images/watermark.png')
+    width, height = base_image.size
+
+    transparent = Image.new('RGB', (width, height), (0, 0, 0, 0))
+    transparent.paste(base_image, (0, 0))
+    transparent.paste(watermark, (0, 0), mask=watermark)
+    transparent.show()
+    img_path = 'media/items/{}/{}'.format(instance.item.id, str(uuid.uuid4()) + '_water.jpg')
+    transparent.save(img_path, 'JPEG', quality=75)
+
+    image = Image.open(instance.image)
+
+    os.makedirs('media/items/{}'.format(instance.item.id), exist_ok=True)
+    if image.mode in ('RGBA', 'LA'):
+        background = Image.new(image.mode[:-1], image.size, fill_color)
+        background.paste(image, image.split()[-1])
+        image = background
+    image.thumbnail((400, 400), Image.ANTIALIAS)
+    small_name = 'media/items/{}/{}'.format(instance.item.id, str(uuid.uuid4()) + '.jpg')
+    if settings.DEBUG:
+        image.save(small_name, 'JPEG', quality=75)
+    else:
+        image.save('laskshmi/' + small_name, 'JPEG', quality=75)
+    instance.image_small = '/' + small_name
+
+#post_save.connect(ItemImage_post_save, sender=ItemImage)
---
+++ static/js/main.js
@@ -0,0 +1,300 @@
+Vue.component('header-cart-items', {
+    delimiters: ['[[', ']]'],
+    props: ['item','index'],
+    template:`    <div class="header-cart-inner-item">
+                            <img :src="[[item.image]]" :alt="[[item.name]]">
+                            <div class="header-cart-inner-item-info">
+                                <p class="header-cart-inner-item-info-name">[[item.name]]</p>
+                                <div class="header-cart-inner-item-info-price">
+                                    <p class="header-cart-inner-item-info-price-p1">Цена: <span>[[item.price]] руб.</span></p>
+                                    <p class="header-cart-inner-item-info-price-p2">Итого: <span>[[item.num * item.price]] руб.</span></p>
+                                </div>
+                            </div>
+                            <div class="header-cart-inner-item-delete">
+                                <span v-on:click="userDelete(index)">&#10006;</span>
+                            </div>
+                        </div>`,
+    methods: {
+        userDelete: function(index){
+            this.$emit('userdelete', index);
+        }
+    }
+})
+
+
+Vue.component('main-cart-item', {
+    delimiters: ['[[', ']]'],
+    props: ['item','index'],
+    template:` <div class="cart-items__item block-w-r4 cart-grid">
+                        <div class="cart-items__item-name">
+                            <img :src="[[item.image]]" alt="">
+                            <div class="cart-items__item-name-wrapper">
+                                <p class="text-bold mb-10">[[item.name]]</p>
+                                <span>Амулеты Фэн-Шуй</span>
+                            </div>
+                        </div>
+                        <div style="flex-basis: 125px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;" class="cart-items__item-qt">
+                             <p v-on:click="delQt(index)">-</p><input class="form-control" id="quantity" readonly type="number" :value="[[item.num]]" min="1"><p v-on:click="addQt(index)">+</p>
+                        </div>
+                        <div class="cart-items__item-price text-bold">[[item.price]]Р</div>
+                        <div class="cart-items__item-total text-bold">[[item.num * item.price]]Р</div>
+                        <div class="cart-items__item-action  text-bold"><span v-on:click="userDelete(index)">&#215;</span></div>
+
+                    </div>`,
+    methods: {
+        userDelete: function (index) {
+            this.$emit('userdelete', index);
+        },
+        delQt: function (index) {
+
+            this.$emit('del_qt', index);
+        },
+        addQt: function (index) {
+            this.$emit('add_qt', index);
+        }
+    }
+})
+
+var app = new Vue({
+    delimiters: ['[[', ']]'],
+    el: '#app',
+    data: {
+        cartTotal:0,
+        cartTotalWithDiscount:0,
+        cartItemsNum:0,
+        promoDiscount:0,
+        promo_code:'',
+        headerCartOpen:false,
+        cartNotEmpty : false,
+        loginModal:false,
+        menuOpen:false,
+        registerModal:false,
+        headerCartItems: [
+        ]
+    },
+
+    methods:{
+        openMenu:function(){
+          if (this.menuOpen){
+              this.menuOpen = false
+          }else {
+              this.menuOpen = true
+          }
+
+        },
+        openModal: function(id){
+          this.loginModal = false
+          this.registerModal = false
+            if(id==='reg'){
+              this.registerModal = true
+            }
+            if(id==='log'){
+              this.loginModal = true
+            }
+        },
+        headerCartMouseLeave: function(){
+            this.headerCartOpen=false
+        },
+        remove: function(index){
+            let item_id =this.headerCartItems[index]['id']
+            this.headerCartItems.splice(index, 1)
+            this.sendUpdateRequest(item_id,'del_item')
+            Toastify({
+                duration: 1000,
+                close: true,
+                text: `Товар удален из корзины`,
+                backgroundColor: "linear-gradient(to right, #f55f63, #be353b)",
+                className: "info",
+            }).showToast();
+        },
+        openHeaderCart:function () {
+            if (this.headerCartOpen){
+                this.headerCartOpen=false
+            }else{
+                this.headerCartOpen=true
+            }
+        }
+        ,
+        del_qt: function (index) {
+            if (this.headerCartItems[index]['num'] > 1){
+                this.headerCartItems[index]['num'] -= 1
+                this.updateCart(this.headerCartItems[index]['id'],this.headerCartItems[index]['num'])
+            }
+        },
+        add_qt: function (index) {
+            this.headerCartItems[index]['num'] += 1
+            this.updateCart(this.headerCartItems[index]['id'],this.headerCartItems[index]['num'])
+        },
+        updateCart: function (item_id,num) {
+            console.log('update',item_id,num)
+            this.cartTotal = 0
+            for(var item in this.headerCartItems){
+                console.log(this.headerCartItems[item]['price'])
+                this.cartTotal +=  parseInt(this.headerCartItems[item]['price']) * parseInt(this.headerCartItems[item]['num'])
+            }
+            if (this.promoDiscount > 0){
+                this.cartTotalWithDiscount = parseInt(this.cartTotal - (this.cartTotal * this.promoDiscount / 100))
+            }
+
+            this.sendUpdateRequest(item_id,'set_num',num)
+        },
+        sendUpdateRequest: function(item_id,action,num='1'){
+            let csrfmiddlewaretoken = document.getElementsByName('csrfmiddlewaretoken')[0].value,
+                overlay = document.getElementById('cart_overlay'),
+                body = {item_id:item_id,
+                    action:action,
+                    number:num}
+            if (overlay){overlay.classList.add('cart-overlay-active')}
+            console.log(body)
+            fetch(`/cart/add_to_cart/`, {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: { "X-CSRFToken": csrfmiddlewaretoken },
+                credentials: 'same-origin'
+            }).then(res=>res.json())
+                .then(res => {
+                    if (res['result'] === true){
+                        console.log(res)
+                    }
+                    if (overlay){overlay.classList.remove('cart-overlay-active')}
+
+                })
+        },
+        addItem: function (event) {
+            let item_id = event.target.getAttribute('data-id'),
+                item_name = event.target.getAttribute('data-name'),
+                item_price = event.target.getAttribute('data-price'),
+                item_image = event.target.getAttribute('data-image'),
+                item_num = event.target.getAttribute('data-num'),
+                id_in_array = false,
+                index = 0
+
+            console.log('add')
+
+            let values = Object.entries(this.headerCartItems )
+            console.log('en',values)
+            for (x of values){
+                console.log('xx',x[1]['id'])
+                if (parseInt(item_id) === parseInt(x[1]['id'])){
+                    id_in_array = true
+                    index=x[0]
+                }
+            }
+
+            if (id_in_array){
+                console.log('in array')
+                this.headerCartItems[parseInt(index)]['num'] = parseInt(item_num)
+                console.log(this.headerCartItems)
+                this.updateCart(this.headerCartItems[parseInt(index)]['id'],this.headerCartItems[parseInt(index)]['num'])
+            }
+            else {
+                this.headerCartItems.push({id:item_id,name:item_name,num:item_num,price:item_price,image:item_image })
+                this.sendUpdateRequest(item_id,'add_new',item_num)
+            }
+            Toastify({
+                duration: 1000,
+                close: true,
+                text: `${item_name} ${item_num}шт. добавлено в корзину`,
+                backgroundColor: "linear-gradient(to right, #f55f63, #be353b)",
+                className: "info",
+            }).showToast();
+        },
+        addInFav:function (event) {
+            console.log('addinfav')
+            let csrfmiddlewaretoken = document.getElementsByName('csrfmiddlewaretoken')[0].value,
+                body = {item_id:event.target.getAttribute('data-id')},
+                text=''
+
+
+            console.log(body)
+            fetch(`/cart/add_to_fav/`, {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: { "X-CSRFToken": csrfmiddlewaretoken },
+                credentials: 'same-origin'
+            }).then(res=>res.json())
+                .then(res => {
+
+                      console.log(res)
+                       event.target.classList.toggle('item-in-fav')
+                    if (res['result']==='deleted'){text='Товар удален из избранного'}
+                    if (res['result']==='added'){text='Товар добавлен в избранное'}
+
+                    Toastify({
+                        duration: 1000,
+                        close: true,
+                        text: text,
+                        backgroundColor: "linear-gradient(to right, #f55f63, #be353b)",
+                        className: "info",
+                    }).showToast();
+                })
+        },
+        changeImg:function (event) {
+            let large_img = event.target.dataset.large,
+                small_img = event.target.dataset.small,
+                zoom_large = document.getElementById('zoom_img_large'),
+                zoom_small = document.getElementById('zoom_img_small')
+
+            zoom_large.style.backgroundImage = `url(${large_img})`
+            zoom_small.setAttribute('src',small_img)
+
+
+        },
+        usePromo:function (event) {
+            if(this.promo_code===''){return}
+             console.log('promo',this.promo_code)
+              let csrfmiddlewaretoken = document.getElementsByName('csrfmiddlewaretoken')[0].value,
+                body = {promo_code:this.promo_code}
+
+
+            console.log(body)
+            fetch(`/cart/use_promo/`, {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: { "X-CSRFToken": csrfmiddlewaretoken },
+                credentials: 'same-origin'
+            }).then(res=>res.json())
+                .then(res => {
+
+                      console.log(res)
+
+
+                    Toastify({
+                        duration: 1000,
+                        close: true,
+                        text: res['result'],
+                        backgroundColor: "linear-gradient(to right, #f55f63, #be353b)",
+                        className: "info",
+                    }).showToast();
+                })
+
+        }
+    },
+    watch: {
+        promoDiscount:function(val){
+          console.log('apply discount')
+            this.cartTotalWithDiscount = parseInt(this.cartTotal - (this.cartTotal * this.promoDiscount / 100))
+        },
+        headerCartItems: function (val) {
+            console.log('change')
+            this.cartTotal = 0
+            let x = 0,
+                cart_items_count = document.getElementById('cart_items_count')
+
+            for(var item in val){
+                console.log(val[item]['price'])
+                this.cartTotal +=  parseInt(val[item]['price']) * parseInt(val[item]['num'])
+                x+=1
+            }
+            if (this.promoDiscount > 0){
+                this.cartTotalWithDiscount = parseInt(this.cartTotal - (this.cartTotal * this.promoDiscount / 100))
+            }
+            console.log('items count=', x)
+            this.cartItemsNum = x
+
+        }
+    }
+})
