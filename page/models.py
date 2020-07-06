5949d93a879543b850128f04d185cd87ef724974
ddnnss <ddnnss.i1@gmail.com>
2020-07-06 19:55:44 +0300

    css fix

--- page/models.py
+++ page/models.py
@@ -3,8 +3,11 @@ from django.db import models
 class Banner(models.Model):
     order = models.IntegerField('Порядок отображения', default=1)
     image = models.ImageField('Картинка', upload_to='banners/', blank=False)
-    url = models.CharField('Ссылка', max_length=255, blank=False)
+    title = models.CharField('Заголовок', max_length=255, blank=True)
+    subtitle = models.CharField('Описание', max_length=255, blank=True)
+    url = models.CharField('Ссылка', max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
+
 
     def __str__(self):
         return 'Баннер, порядковый номер : %s' % self.order
